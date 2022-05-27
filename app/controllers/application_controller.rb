3194362bab82b52c267e578d7cbd305199ae7fee
bruna suzuki <brunasuzuki12@gmail.com>
2022-05-27 09:22:39 -0700

    rails new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  def index
-    @tasks = Task.all
-  end
-
-  def create
-    @task = Task.new(params[:task])
-    @task.save
-  end
-
-  def show
-    @task = Task.find(params[:id])
-  end
-
-  def new
-    @task = Task.new
-  end
 end
